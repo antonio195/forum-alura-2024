361e26a65f6eb8410933fba030b1551d6d01d977
antonio195 <antonio195br@gmail.com>
2024-11-18 19:24:50 -0300

    Projeto base

---
+++ src/main/kotlin/com/antoniocostadossantos/ForumAlura2024Application.kt
@@ -0,0 +1,11 @@
+package com.antoniocostadossantos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForumAlura2024Application
+
+fun main(args: Array<String>) {
+	runApplication<ForumAlura2024Application>(*args)
+}
